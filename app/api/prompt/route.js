9d80d72c32b0cf6dbd8b7cd5049e11f9adf5b7c1
Anjali Vibhute <101789772+VibhuteAnjali@users.noreply.github.com>
2024-08-01 21:23:57 +0530

    Update route.js

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -4,7 +4,7 @@ import { connectToDB } from "@utils/database";
 export const GET = async (req, res) => {
   try {
     await connectToDB();
-    const data = await Prompt.find({}).populate("creator");
+    const data = await Prompt.find().populate("creator");
     return new Response(JSON.stringify(data), { status: 200 });
   } catch (error) {
     console.log(error);
